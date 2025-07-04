07a094e8747e2aaa2b8c018d9837caf035f16b21
Tracy Kabuya <tracykabuya@gmail.com>
2025-07-04 10:20:37 +0200

    added router

---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import OffgridLogo from '../../assets/offgrid-logo.svg?react';
+import './navigation.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <OffgridLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
