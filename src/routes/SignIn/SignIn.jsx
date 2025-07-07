dde5f228450f10ac7a84d62c6f5a28a25773a485
Tracy Kabuya <tracykabuya@gmail.com>
2025-07-07 16:02:22 +0200

    added the logic of the sign up form

--- src/routes/SignIn/SignIn.jsx
+++ src/routes/SignIn/SignIn.jsx
@@ -3,16 +3,20 @@ import {
     createUserDocumentFromAuth
  } from "../../utils/firebase/firebase";
 
+import SignUpForm from '../../components/SignUpForm/SignUpForm'
+
 const SignIn = () => {
-   const logGoogleUser = async () => {
+ const logGoogleUser = async () => {
      const { user } = await signInWithGooglePopup();
      const userDocRef = await createUserDocumentFromAuth(user);
   }
 
+
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign In WIth Google Pop Up</button>
+      <button onClick={logGoogleUser}>Sign In With Google Pop Up</button>
+      <SignUpForm/>
     </div>
    );
 }
