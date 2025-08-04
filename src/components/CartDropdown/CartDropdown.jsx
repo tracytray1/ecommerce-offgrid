aa0d14ffc475a4706586963d3a41536cae84dd1c
Tracy Kabuya <tracykabuya@gmail.com>
2025-08-04 10:56:47 +0200

    updated the cart item as well as the basket

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,15 +1,29 @@
+import { useContext } from 'react'
+
+import { CartContext } from '../../contexts/cart'
+
 import Button from '../Button/Button'
+import CartItem from '../CartItem/CartItem'
 
 import './CartDropdown.scss'
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>Go to pay</Button>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
       </div>
+      <Button>Go to pay</Button>
     </div>
-  )
+  );
 }
 
 export default CartDropdown
