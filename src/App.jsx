a0e01580121147bcb6d058660436059fea7394cb
Tracy Kabuya <tracykabuya@gmail.com>
2025-08-04 13:21:43 +0200

    finished working on the checkout page adding cart total

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+
 import Home from './routes/Home/Home';
 import Navigation from './routes/Navigation/Navigation';
 import Authentication from './routes/Authentication/Authentication';
-import { Routes, Route } from 'react-router-dom';
 import Shop from './routes/Shop/Shop'
+import Checkout from './routes/Checkout/Checkout'
+
 
 
 const App = () => {
@@ -12,6 +16,8 @@ const App = () => {
         <Route index element={ <Home /> } />
         <Route path='shop' element={ <Shop /> } />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
+
       </Route>
     </Routes>
   );
